511e774b6ee5e9b85661a2c3dd943570c378fbd7
Maxim <hristivi8383@gmail.com>
2021-12-29 21:30:48 +0200

    path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 const connectDb = require('./db');
 
@@ -7,7 +8,7 @@ connectDb();
 app.use(express.static('../web'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.static(path.join(__dirname, './frotnend/build')));
+app.use(express.static(path.join(__dirname, '/frotnend/build')));
 
 app.use('/admin', require('./Routes/adminRoute'));
 app.use('/article', require('./Routes/articleRoute'));
