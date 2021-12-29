1e152850e5c6e550f64b0c661c346b2b46182f3e
Maxim <hristivi8383@gmail.com>
2021-12-29 22:31:30 +0200

    changeFolder

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const connectDb = require('./db');
 
 connectDb();
-app.use(express.static('../web'));
+app.use(express.static('./frontend'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, '/frotnend/build')));
