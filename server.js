8d70ce6702d7416a830074d1b6c0bf3f5e7cb805
Maxim <hristivi8383@gmail.com>
2021-12-29 22:41:24 +0200

    final

--- server.js
+++ server.js
@@ -9,14 +9,13 @@ app.use(express.static('./frontend'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use('/admin', require('./Routes/adminRoute'));
+app.use('/article', require('./Routes/articleRoute'));
+app.use('/category', require('./Routes/categoryRoute'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 })
-
-app.use('/admin', require('./Routes/adminRoute'));
-app.use('/article', require('./Routes/articleRoute'));
-app.use('/category', require('./Routes/categoryRoute'));
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
