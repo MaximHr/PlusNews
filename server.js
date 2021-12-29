aa5ecbee4aa97edf9c54376d23d84655961f7543
Maxim <hristivi8383@gmail.com>
2021-12-29 22:34:35 +0200

    misspell

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connectDb();
 app.use(express.static('./frontend'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.static(path.join(__dirname, '/frotnend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
