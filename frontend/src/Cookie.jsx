140498e806ba70aed9384201c2d2dd3e5d8ef813
Maxim <hristivi8383@gmail.com>
2022-01-12 22:25:56 +0200

    localstorage

--- frontend/src/Cookie.jsx
+++ frontend/src/Cookie.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import cookie from './images/cookie.png';
 
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-
 const Cookie = ({setShowCookie}) => {
     const acceptHandler = () => {
         setShowCookie(false);
-        setCookie('cookie', true);
+        localStorage.setItem('cookie', true);
     }
     return( 
         <div className="cookie">
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,17 +4,6 @@ import axios from 'axios';
 import Card from './Card';
 import Cookie from './Cookie';
 
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
 const Home = ({categories}) => {
     const [articles, setArticles] = useState([]);
     const [showCookie, setShowCookie] = useState(false);
@@ -24,8 +13,8 @@ const Home = ({categories}) => {
             .then((res) => {
                 if(res.status === 200) {
                     setArticles(res.data);
-                    const getCookieAlert = getCookie('cookie')
-                    if(getCookieAlert === null) {
+                    const getCookie = localStorage.getItem('cookie');
+                    if(getCookie === null) {
                         setShowCookie(true);
                     }
                 }
